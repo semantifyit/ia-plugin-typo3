9a88237ece154be486e5a1b05ae29a5d30ada5c8
Richi <generator@mydevelops.com>
2018-11-13 13:11:06 +0100

    NEW: new version

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.1.8',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.5.99',
+            'typo3' => '6.2.0-9.5.99',
         ),
         'conflicts' => array(
             'mayrhofen_annotator' => '0.1.0-9.9.9',
