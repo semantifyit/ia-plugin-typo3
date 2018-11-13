36311e766f2eb708f6d81b9ede398d1d43399dd5
Richi <generator@mydevelops.com>
2018-11-13 13:10:23 +0100

    NEW: db support

---
+++ ext_tables.sql
@@ -0,0 +1,14 @@
+#
+# Modifying pages table
+#
+
+CREATE TABLE pages (
+iasemantify_ann_id text DEFAULT '' NOT NULL
+);
+
+#
+# Modifying pages_language_overlay table
+#
+CREATE TABLE pages_language_overlay (
+  iasemantify_ann_id text DEFAULT '' NOT NULL
+);
