79f455645ade424d1142f9c6e21deb5b8b1260d3
Richi <generator@mydevelops.com>
2018-11-20 11:41:49 +0100

    NEW: new support for calling frontend action

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'STI.' . $_EXTKEY,
-    'InstantAnnotations',
-    array(
-        'InstantAnnotations' => 'list, ajaxHandler',
-    ),
-    // non-cacheable actions
-    array(
-        'InstantAnnotations' => 'list, ajaxHandler',
-    )
-);
-*/
+// HOOK is called after caching
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'EXT:' . $_EXTKEY . '/Classes/class.tx_annotation_input.php:&tx_annotation_input->performNotCached';
+
+// HOOK is called before caching
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:'. $_EXTKEY .'/Classes/class.tx_annotation_input.php:&tx_annotation_input->performCached';
