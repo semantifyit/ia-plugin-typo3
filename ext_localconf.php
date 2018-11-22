8f8b4e09fc80a80849611c44f9c6a56fe84bbb64
Richi <generator@mydevelops.com>
2018-11-22 08:46:17 +0100

    NEW: resolving url

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,6 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-outp
 
 // HOOK is called before caching
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:'. $_EXTKEY .'/Classes/class.tx_annotation_input.php:&tx_annotation_input->performCached';
+
+//page path
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Vendor/typo3-pagepath/class.tx_pagepath_resolver.php';
