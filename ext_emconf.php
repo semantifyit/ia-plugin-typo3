cb203475683ddc3850078636f1134da644dd65cf
Richi <generator@mydevelops.com>
2019-05-02 21:00:09 +0200

    NEW: updated support for newer typo3 versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,10 +13,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.3',
+    'version' => '1.1.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-9.5.99',
+            'typo3' => '6.2.0-19.9.99',
         ),
         'conflicts' => array(
             'mayrhofen_annotator' => '0.1.0-9.9.9',
