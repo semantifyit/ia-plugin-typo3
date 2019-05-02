124bb90689c0e37d6f82069211091afca330823e
Richi <generator@mydevelops.com>
2019-05-02 21:09:25 +0200

    FIX: fixed lower version

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.1.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-19.9.99',
+            'typo3' => '7.2.0-19.9.99',
         ),
         'conflicts' => array(
             'mayrhofen_annotator' => '0.1.0-9.9.9',
