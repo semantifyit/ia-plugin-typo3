d763291e6449bb4e3851a6ff83efa64a46b8478f
Richi <generator@mydevelops.com>
2019-05-08 18:08:59 +0200

    FIX: added maximal highes version of typo3

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.1.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-10.9.99',
+            'typo3' => '6.2.0-9.5.99',
         ),
         'conflicts' => array(
             'mayrhofen_annotator' => '0.1.0-9.9.9',
@@ -25,4 +25,5 @@ $EM_CONF[$_EXTKEY] = array(
         'suggests' => array(
         ),
     ),
+
 );
